297608de3d7bd3e649f55d18c705255663cf3d17
hashim7676 <96419980+hashim7676@users.noreply.github.com>
2022-01-06 02:45:30 +0500

    Final Upload For Project
    Attendance System added. This concludes the additions to the code and this is the final version of the project.
    No further uploads.

---
+++ Project_Final.py
@@ -0,0 +1,82 @@
+import face_recognition
+import cv2
+import numpy as np
+import os
+from datetime import datetime
+
+###########################################################################
+
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+video_capture = cv2.VideoCapture(0)
+
+files = os.listdir(r"C:\Work\Python_Project\images")
+
+known_face_encodings = []
+
+for file in files:
+    image = face_recognition.load_image_file(f"images\{file}")
+    face_encoding = face_recognition.face_encodings(image)[0]
+    known_face_encodings.append(face_encoding)
+
+path = "C:\Work\Python_Project\images"
+known_face_names = [os.path.splitext(filename)[0] for filename in os.listdir(path)]
+
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True:
+    ret, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    if process_this_frame:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            face_names.append(name)
+
+            if name != "Unknown":
+                markAttendance(name)
+
+    process_this_frame = not process_this_frame     # Same as setting it to 'False'
+
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 12), font, 0.50, (255, 255, 255), 1)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
